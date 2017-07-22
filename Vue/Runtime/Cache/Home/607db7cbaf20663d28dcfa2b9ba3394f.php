273258e0363836bfc37d9deb5c85b0542e05ec1a
XLongGitHub <1311620554@qq.com>
2017-07-22 13:44:27 +0800

    用户注册主页

--- Vue/Runtime/Cache/Home/607db7cbaf20663d28dcfa2b9ba3394f.php
+++ Vue/Runtime/Cache/Home/607db7cbaf20663d28dcfa2b9ba3394f.php
@@ -4,12 +4,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    
-        <title>标题</title>
-    
-    <link href="../../../Public/css/user-style.css" rel="stylesheet">
+    <title>reigster</title> 
+    <link href="../../../Public/css/base.css" rel="stylesheet"> 
+    <link href="../../../Public/css/user.css" rel="stylesheet">
     <link href="../../../Public/css/header.css" rel="stylesheet">
     <link href="../../../Public/css/footer.css" rel="stylesheet">
+    <link href="../../../Public/css/left.css" rel="stylesheet">
     <style>
 
     </style>
@@ -43,18 +43,24 @@
             </div>
         </div>
     
-    menu
-    left
+    <!-- menu -->
     
-     <div id="wrap">
-        <div></div>
-        <form>
+        <div id="left">left </div>
+        
+    
+<link href="../../../Public/css/user.css" rel="stylesheet">  
+     <div id="user-add">
+        <div class="welcome">
+            欢迎注册GEEK论坛，在这里你可以向大家展示你的才华，找到更多志同道合的人！
+        </div>
+        <form id="user-add-form">
             <div>
-                <label>姓名:</label>
+                <label class="input-tip">姓名:</label>
                 <input name="name" type="text" placeholder="请输入你的姓名"/>
             </div>
+            <br/>
             <div>
-                <label>性别:</label>
+                <label class="input-tip">性别:</label>
                 <div id="gender">
                     <div>
                         <input type="radio" name="gender" id="male" value="male"/>
@@ -68,13 +74,58 @@
                         <input type="radio" name="gender" id="secret" value="secret"/>
                         <label for="secret">保密</label>
                     </div>
+                </div>
+            </div>
+            <br/>
+            <div>
+                <label class="input-tip">年龄:</label>
+                <input type="text" name="age" placeholder=""/>
+            </div>
+            <br/>
+            <div>
+                <label class="input-tip">生日:</label>
+                <input type="date" name="birthday"/>
+            </div>
+            <br/>
+            <div>
+                <label class="input-tip">地址:</label>
+                <input type="text" name="brithday"/>
+            </div>
+            <br/>
+            <div>
+                <label class="input-tip">电话:</label>
+                <input type="number" name="phone"/>
+            </div>
+            <br/>
+            <div>
+                <label class="input-tip">邮箱:</label>
+                <input type="email" name="email"/>
+            </div>
+            <br/>
+            <div>
+                <label class="input-tip">简介:</label>
+                <textarea name="introduction">用几句话，简单描述自己！</textarea>
+            </div>
+            <br/>
+            <div>
+                <!-- <input type="submit" class="submit" value="注册"/> -->
+                <!-- <input type="button" class="back" value="返回"/> -->
+                <button type="submit" class="submit">注册</button>
+                <button class="back">返回</button>
+            </div>
+            
+        
                 
             </form>
         <?php  ?>
         </div>
 
-    footer
-        <!-- <div id="footer"></div> -->
+    <br/>
+    
+        <br/>
+          <div id="footer">
+              ©2017 Geek 
+              </div>  
     
 </body>
 
