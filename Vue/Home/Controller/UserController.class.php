ad143c53e6dfeea3b3cf2e72cf73ee1dfe81b7f8
1311620554@qq.com <email@example.com>
2017-07-23 13:02:44 +0800

    完善用户注册后台代码

--- Vue/Home/Controller/UserController.class.php
+++ Vue/Home/Controller/UserController.class.php
@@ -8,28 +8,33 @@ class UserController extends Controller {
         $User = D('users');
         $users = $User->select();
         $this->assign('users', $users);
-        // var_dump($users);
         $this->display();
     }
-
+    /**
+    *用户使用电话号码进行注册
+    */
     public function register() {
-        if (IS_GET) {
+        if ($_SERVER['HTTP_REQUEST_TYPE'] == 'ajax') {
+            exit();
+        } else if (IS_GET) {
             $password =trim(I('get.password', ''));
             if (!empty($password)) { 
-                    echo $phone;
                     $this->success();
-                    echo 1;
+                    exit();
             } else {
-                    echo 2;
                     $this->display('register');
             }
         } else if (IS_POST) {
-            echo 3;
             $phone = trim(I('post.phone', '', 'strval'));
             $password = trim(I('post.password', '', 'strval'));
+            if (empty($phone) || empty($password)) {
+                $this->error();
+                exit;
+            }
             $data = array(
                 'phone' => $phone,
-                'password' => $password
+                'password' => md5($password),
+                'create_time' => date('Y/m/d H:i:sa')
             );
             $Users = D('users');
             $id = $Users->data($data)->add();
@@ -39,8 +44,9 @@ class UserController extends Controller {
                 $this->error();
             }
 
+        } else {
+            $this->display('register');
         }
-        // echo 'afdf';
         // $this->display('register');  //此处每次请求后，刷新页面
     }
 
@@ -57,6 +63,10 @@ class UserController extends Controller {
     }
 
     public function gerUser() {
+
+    }
+
+    public function getSms() {
 
     }
 }
